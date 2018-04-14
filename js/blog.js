408b115d7d153c7bb668eb12a9fed730c39293be
guidancegroup <37658320+guidancegroup@users.noreply.github.com>
2018-04-14 18:28:59 +0530

    Update blog.js

--- js/blog.js
+++ js/blog.js
@@ -4,7 +4,7 @@ var blog = angular.module('myBlog', [ 'ngRoute','ngCookies' ]);
 blog.config([ '$routeProvider', function($routeProvider) {
 	
 	$routeProvider.when('/home', {
-		templateUrl : 'blogs/main.html',
+		templateUrl : 'blogs/Home.html',
 	})
 	.otherwise({
 		redirectTo : '/home'
