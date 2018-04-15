b6d0542c0a9675648f63959fa2a588d85265cbd1
guidancegroup <37658320+guidancegroup@users.noreply.github.com>
2018-04-15 23:11:58 +0530

    new js

--- js/blog.js
+++ js/blog.js
@@ -1,19 +1,52 @@
-var blog = angular.module('myBlog', [ 'ngRoute','ngCookies' ]);
+
+var blog = angular.module('myBlog', [ 'ngRoute']);
 //========================================================================================================================================
 
 blog.config([ '$routeProvider', function($routeProvider) {
 	
-	$routeProvider
-	.when('/home', {
-		templateUrl : 'blogs/Home.html',
+	$routeProvider.when('/read', {
+		templateUrl : 'blogs/blog.html',
+	}).when('/blogs', {
+		templateUrl : 'blogs/BlogsList.html',
+		controller:'blogControl'
 	})
 	.otherwise({
-		redirectTo : '/home'
+		redirectTo : '/blogs'
 	});
 } ]);
 
-blog.controller('blogControl', function($rootScope,$scope,$cookies) {
+blog.filter('trustUrl', function ($sce) {
+    return function(url) {
+    	url='blogs/'+url+'.txt';
+        return $sce.trustAsResourceUrl(url);
+      };
+   });
+
+blog.controller('blogControl', function($rootScope,$scope) {
 	
-	console.log("working");
+	$scope.readBlog=function(blog){
+		$rootScope.currentBlog=blog;
+	}
+	//Blog Entry
+	$rootScope.blogsData={
+			"blogs":[
+				{
+					"name":"sample",
+					"summary":"this  is a sample blog for guidance group awesome its working",
+					"fileName":"sample",
+					"publishDate":"12/4/2018",
+					"blogImage":"sample"
+				},
+				{
+					"name":"sample2",
+					"summary":"this  is a SECOND sample  blog for guidance group awesome its working",
+					"fileName":"sample2",
+					"publishDate":"16/4/2018",
+					"blogImage":"sample"
+				}
+			]
+	};
 	
 });
+
+
